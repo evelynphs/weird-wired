a6e419db69ad82cbd5b59cf817df990c1ebde75a
evelynphs <evelyn.p.h.silalahi@gmail.com>
2023-09-12 02:51:56 +0700

    create repository

---
+++ weird_wired/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for weird_wired project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weird_wired.settings')
+
+application = get_asgi_application()
